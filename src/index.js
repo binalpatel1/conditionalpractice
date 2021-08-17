706261102468480f825b48201ef2314409e29941
binal <69486543+binalpatel1@users.noreply.github.com>
2021-08-16 18:37:21 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//Challenge: Without moving the userIsRegistered variable,
